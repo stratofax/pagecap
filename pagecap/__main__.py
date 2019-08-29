71c66a11f23117a883b32e738dfb186d586240dd
Neil Johnson <neil@cadent.com>
2019-08-29 15:05:19 -0400

    Check if run from CLI

--- pagecap/__main__.py
+++ pagecap/__main__.py
@@ -1,9 +1,29 @@
 """
-Handles CLI or use as library
+Handles CLI or use as library module
 """
+
+# import click
 
 import pagecap_metadata as pcmeta
 
-print(pcmeta.__title__)
-print('Version ' + pcmeta.__version__)
-print(pcmeta.__description__)
+"""
+# requires click
+@click.command()
+@click.argument('url', nargs=-1)
+@click.option(
+    '--url_file',
+    type=click.File('r'),
+    required=False,
+)
+"""
+
+# def capture_page(url_file, url):
+
+def capture_page():
+    print(pcmeta.__title__)
+    print('Version ' + pcmeta.__version__)
+    print(pcmeta.__description__)
+
+# if running as a script from the CLI
+if __name__ == '__main__':
+    capture_page()
