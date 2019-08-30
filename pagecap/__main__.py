0a9d460f81742968e5ab48cc4562dbe9f56d1c13
Neil Johnson <neil@cadent.com>
2019-08-30 13:27:24 -0400

    Capture full page headless mode

--- pagecap/__main__.py
+++ pagecap/__main__.py
@@ -2,20 +2,22 @@
 Handles CLI or use as library module
 """
 
+# external libraries
 import click
 
 # get the latest chromedriver here:
 # https://sites.google.com/a/chromium.org/chromedriver/downloads
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.options import Options
 
+# local package imports
 import pagecap_metadata as pcmeta
 
 @click.command()
 @click.version_option(pcmeta.__version__)
 @click.argument('urls', nargs=-1)
 @click.option(
-    '-f', 
+    '-l', 
     '--urlfile',
     type=click.File('r'),
     required=False,
@@ -33,13 +35,36 @@ def capture_page(urlfile, urls):
         add_s = ''
         page_count = len(urls) 
         if page_count > 1: add_s = 's'
-        args_listed = ('URL{} specified: {}'.format(add_s, page_count))
-        click.echo(args_listed)
-        click.echo(urls)
+        how_many = ('URL{} specified: {}'.format(add_s, page_count))
+
+        click.echo(how_many)
+        # click.echo(urls)
+        
+        for url in urls:
+            click.echo(url)
+            make_png(url)
+
     
     else:        # no URLs provided
         click.echo('Please enter at least one URL to capture!')
         click.echo('Use --help for a list of options.')
+
+def make_png(capture_url):
+    """ create a PNG file from the URL provided 
+        from https://stackoverflow.com/questions/41721734/take-screenshot-of-full-page-with-selenium-python-with-chromedriver/57338909#57338909
+    """
+    options = webdriver.ChromeOptions()
+    options.headless = True
+
+    driver = webdriver.Chrome(options=options)
+    driver.get(capture_url)
+
+    S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
+    driver.set_window_size(S('Width'),S('Height')) # May need manual adjustment
+    driver.find_element_by_tag_name('body').screenshot('web_screenshot.png')
+
+    driver.quit()
+
 
 # if running as a script from the CLI
 if __name__ == '__main__':
